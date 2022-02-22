a7c253918a8f00061b743bf475d972767c86b0ac
Daniel <danarturodo@hotmail.com>
2022-02-22 00:11:03 -0500

    fix new

--- src/app/modules/contactanos/contactanos.component.ts
+++ src/app/modules/contactanos/contactanos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactanos.component.scss']
 })
 export class ContactanosComponent implements OnInit {
-  panelOpenState = false;
+
   constructor() { }
 
   ngOnInit() {
